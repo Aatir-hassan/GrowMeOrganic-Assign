27cb668e049bd8857dcd4b46091b4b95768e0a31
Mr Aatir <piraatir241@gmail.com>
2025-10-03 21:05:32 +0530

    updated App

--- react-artworks/src/App.tsx
+++ react-artworks/src/App.tsx
@@ -6,8 +6,10 @@ import { fetchArtworks } from './api'
 import type { Artwork } from './api'
 import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
+
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+
 import './App.css'
 
 type SelectionState = {
